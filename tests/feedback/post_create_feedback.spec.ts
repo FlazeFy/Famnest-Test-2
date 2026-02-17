3e67672bc92062cc97cc1f2f7547030b2ddda1b3
FlazeFy2 <flazen.edu@gmail.com>
2026-02-18 03:28:32 +0700

    test(question): add integration tests for POST question API (1 success, 4 failure cases)
    The last commit accidentally got swapped with this API integration test LOL

--- tests/feedback/post_create_feedback.spec.ts
+++ tests/feedback/post_create_feedback.spec.ts
@@ -1,91 +1,106 @@
 import { test, expect } from '@playwright/test'
 import { expectDefaultResponseProps, expectKeyExist } from 'helpers/assertions.helper'
+import { useLogin } from 'helpers/auth.helper'
 
-test.describe('Question API - POST Question', () => {
+test.describe('Feedback API - POST Feedback', () => {
     // Test Data
     const loremData = {
         short: process.env.LOREM_SHORT,
         medium: process.env.LOREM_MEDIUM,
     }
+    const url = '/api/v1/feedbacks'
+    let userToken: string
+    let adminToken: string
 
-    const userData = {
-        email: process.env.USER_EMAIL,
-    }
-
-    const url = '/api/v1/questions'
+    test.beforeAll(async ({ request }) => {
+        userToken = await useLogin(request, 'user')
+        adminToken = await useLogin(request, 'admin')
+    })
 
     test.describe('Success cases', () => {
-        test('should send question successfully with valid payload', async ({ request }) => {
+        test('should send feedback successfully with valid payload', async ({ request }) => {
             // Exec the API
             const res = await request.post(url, {
+                headers: {
+                    Authorization: `Bearer ${userToken}`,
+                },
                 data: {
-                    question: loremData.short,
-                    email: userData.email,
+                    feedback_rate: 9,
+                    feedback_body: loremData.short,
                 },
             })
-            const body = await res.json()
 
             // Validate message and data
-            expectDefaultResponseProps(res, 201, 'Question sended')
+            expectDefaultResponseProps(res, 201, 'Feedback sended')
         })
     })
 
     test.describe('Failed cases', () => {
+        test('should fail if the feedback is sent by admin', async ({ request }) => {
+            // Exec the API
+            const res = await request.post(url, {
+                headers: {
+                    Authorization: `Bearer ${adminToken}`,
+                },
+                data: {
+                    feedback_rate: 9,
+                    feedback_body: loremData.short,
+                },
+            })
+
+            // Validate message and data
+            expectDefaultResponseProps(res, 403, 'Your role is not authorized')
+        })
+
         test('should fail with failed validation : payload character length is more than maximum requirement', async ({ request }) => {
             // Exec the API
             const res = await request.post(url, {
+                headers: {
+                    Authorization: `Bearer ${userToken}`,
+                },
                 data: {
-                    question: loremData.medium,
-                    email: userData.email,
+                    feedback_rate: 9,
+                    feedback_body: loremData.medium,
                 },
             })
             const body = await res.json()
 
             // Validate message and data
             expectDefaultResponseProps(res, 422, 'Validation error')
-            expectKeyExist(body.data, ["question"])
-            expect(body.data.question).toBe('question must be at most 255 characters')
-        })
-
-        test('should fail with failed validation : question email is not a valid email', async ({ request }) => {
-            // Exec the API
-            const res = await request.post(url, {
-                data: {
-                    question: loremData.short,
-                    email: loremData.medium,
-                },
-            })
-            const body = await res.json()
-
-            // Validate message and data
-            expectDefaultResponseProps(res, 422, 'Validation error')
-            expectKeyExist(body.data, ["email"])
-            expect(body.data.email).toBe('email must be a valid gmail address')
+            expectKeyExist(body.data, ['feedback_body'])
+            expect(body.data.feedback_body).toBe('feedback_body must be at most 255 characters')
         })
 
         test('should fail with failed validation : required payload is empty', async ({ request }) => {
             // Exec the API
             const res = await request.post(url, {
+                headers: {
+                    Authorization: `Bearer ${userToken}`,
+                },
                 data: {
-                    question: loremData.short,
+                    feedback_rate: 9,
                 },
             })
             const body = await res.json()
 
             // Validate message and data
             expectDefaultResponseProps(res, 422, 'Validation error')
-            expectKeyExist(body.data, ["email"])
-            expect(body.data.email).toBe('email is required')
+            expectKeyExist(body.data, ['feedback_body'])
+            expect(body.data.feedback_body).toBe('feedback_body is required')
         })
 
         test('should fail with failed validation : empty request body', async ({ request }) => {
             // Exec the API
-            const res = await request.post(url)
+            const res = await request.post(url, {
+                headers: {
+                    Authorization: `Bearer ${userToken}`,
+                },
+            })
             const body = await res.json()
 
             // Validate message and data
             expectDefaultResponseProps(res, 422, 'Validation error')
-            expectKeyExist(body.data, ["body"])
+            expectKeyExist(body.data, ['body'])
             expect(body.data.body).toBe('Request body is required')
         })
     })
